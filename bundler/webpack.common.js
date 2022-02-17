3f5fc38ea87f2c856917b2c15f233a7e5001ae19
datalater <the7mincheol@gmail.com>
2022-02-17 14:07:36 +0900

    번들링 옵션에 sass 유틸 자동 사용하도록 추가

--- bundler/webpack.common.js
+++ bundler/webpack.common.js
@@ -1,33 +1,33 @@
-const CopyWebpackPlugin = require("copy-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCSSExtractPlugin = require("mini-css-extract-plugin");
-const path = require("path");
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin')
+const path = require('path')
 
 module.exports = {
-  entry: path.resolve(__dirname, "../src/index.js"),
+  entry: path.resolve(__dirname, '../src/index.js'),
   resolve: {
-    extensions: [".js"],
+    extensions: ['.js'],
     alias: {
-      "@components": path.resolve(__dirname, "../src/components"),
-      "@pages": path.resolve(__dirname, "../src/pages"),
-      "@assets": path.resolve(__dirname, "../src/assets"),
-      "@router": path.resolve(__dirname, "../src/router"),
-      "@utils": path.resolve(__dirname, "../src/utils"),
-      "@store": path.resolve(__dirname, "../src/store"),
+      '@components': path.resolve(__dirname, '../src/components'),
+      '@pages': path.resolve(__dirname, '../src/pages'),
+      '@assets': path.resolve(__dirname, '../src/assets'),
+      '@router': path.resolve(__dirname, '../src/router'),
+      '@utils': path.resolve(__dirname, '../src/utils'),
+      '@store': path.resolve(__dirname, '../src/store'),
     },
   },
   output: {
-    hashFunction: "xxhash64",
-    filename: "bundle.[contenthash].js",
-    path: path.resolve(__dirname, "../dist"),
+    hashFunction: 'xxhash64',
+    filename: 'bundle.[contenthash].js',
+    path: path.resolve(__dirname, '../dist'),
   },
-  devtool: "source-map",
+  devtool: 'source-map',
   plugins: [
     new CopyWebpackPlugin({
-      patterns: [{ from: path.resolve(__dirname, "../static") }],
+      patterns: [{ from: path.resolve(__dirname, '../static') }],
     }),
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "../src/index.html"),
+      template: path.resolve(__dirname, '../src/index.html'),
       minify: true,
     }),
     new MiniCSSExtractPlugin(),
@@ -37,14 +37,14 @@ module.exports = {
       // HTML
       {
         test: /\.(html)$/,
-        use: ["html-loader"],
+        use: ['html-loader'],
       },
 
       // JS
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: ["babel-loader"],
+        use: ['babel-loader'],
       },
 
       // CSS
@@ -52,29 +52,42 @@ module.exports = {
         test: /\.s?css$/i,
         use: [
           MiniCSSExtractPlugin.loader,
-          "css-loader",
-          "postcss-loader",
-          "sass-loader",
+          'css-loader',
+          'postcss-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              additionalData: `
+                @use "sass:color";
+                @use "sass:list";
+                @use "sass:map";
+                @use "sass:math";
+                @use "sass:meta";
+                @use "sass:selector";
+                @use "sass:string";
+              `,
+            },
+          },
         ],
       },
 
       // Images
       {
         test: /\.(jpg|png|gif|svg|glb|gltf)$/,
-        type: "asset/resource",
+        type: 'asset/resource',
         generator: {
-          filename: "assets/images/[hash][ext]",
+          filename: 'assets/images/[hash][ext]',
         },
       },
 
       // Fonts
       {
         test: /\.(ttf|eot|woff|woff2)$/,
-        type: "asset/resource",
+        type: 'asset/resource',
         generator: {
-          filename: "assets/fonts/[hash][ext]",
+          filename: 'assets/fonts/[hash][ext]',
         },
       },
     ],
   },
-};
+}
